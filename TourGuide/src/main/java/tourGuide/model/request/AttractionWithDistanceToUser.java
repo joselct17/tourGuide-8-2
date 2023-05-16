5b4847299c3ef67fab7b98f29e8527dcd61080e3
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-05-16 14:35:49 +0200

    rajout du getter et du setter

--- TourGuide/src/main/java/tourGuide/model/request/AttractionWithDistanceToUser.java
+++ TourGuide/src/main/java/tourGuide/model/request/AttractionWithDistanceToUser.java
@@ -1,7 +1,11 @@
 package tourGuide.model.request;
 
 import gpsUtil.location.Location;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor
+@AllArgsConstructor
 public class AttractionWithDistanceToUser {
 
     private String nameOfTouristAttraction;
@@ -13,4 +17,44 @@ public class AttractionWithDistanceToUser {
     private Double distanceInMilesBetweenTheUsersLocationAndThisAttraction;
 
     private int rewardsPointsForVisitingThisAttraction;
+
+    public String getNameOfTouristAttraction() {
+        return nameOfTouristAttraction;
+    }
+
+    public void setNameOfTouristAttraction(String nameOfTouristAttraction) {
+        this.nameOfTouristAttraction = nameOfTouristAttraction;
+    }
+
+    public Location getLocationOfTouristAttraction() {
+        return locationOfTouristAttraction;
+    }
+
+    public void setLocationOfTouristAttraction(Location locationOfTouristAttraction) {
+        this.locationOfTouristAttraction = locationOfTouristAttraction;
+    }
+
+    public Location getLocationOfUser() {
+        return locationOfUser;
+    }
+
+    public void setLocationOfUser(Location locationOfUser) {
+        this.locationOfUser = locationOfUser;
+    }
+
+    public Double getDistanceInMilesBetweenTheUsersLocationAndThisAttraction() {
+        return distanceInMilesBetweenTheUsersLocationAndThisAttraction;
+    }
+
+    public void setDistanceInMilesBetweenTheUsersLocationAndThisAttraction(Double distanceInMilesBetweenTheUsersLocationAndThisAttraction) {
+        this.distanceInMilesBetweenTheUsersLocationAndThisAttraction = distanceInMilesBetweenTheUsersLocationAndThisAttraction;
+    }
+
+    public int getRewardsPointsForVisitingThisAttraction() {
+        return rewardsPointsForVisitingThisAttraction;
+    }
+
+    public void setRewardsPointsForVisitingThisAttraction(int rewardsPointsForVisitingThisAttraction) {
+        this.rewardsPointsForVisitingThisAttraction = rewardsPointsForVisitingThisAttraction;
+    }
 }
