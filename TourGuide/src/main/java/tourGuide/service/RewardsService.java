eb9e6cb5bc714e675815d88ffad3bf4fb6445021
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-05-26 15:10:55 +0200

    test avec 100 000 users

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -52,7 +52,6 @@ public class RewardsService {
 				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
 					if(nearAttraction(visitedLocation, attraction)) {
 						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-						setRewardsPoint(user, visitedLocation, attraction);
 					}
 				}
 			}
