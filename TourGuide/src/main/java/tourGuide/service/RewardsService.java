3649cfc49cabae4fe6ae30f21b6a24d4d6e94a19
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-05-30 17:50:20 +0200

    modification des commentaires

--- TourGuide/src/main/java/tourGuide/service/RewardsService.java
+++ TourGuide/src/main/java/tourGuide/service/RewardsService.java
@@ -26,7 +26,7 @@ public class RewardsService {
 	private final GpsUtil gpsUtil;
 	private final RewardCentral rewardsCentral;
 
-	private final ExecutorService executorService = Executors.newFixedThreadPool(20);
+	private final ExecutorService executorService = Executors.newFixedThreadPool(60);
 
 
 	public RewardsService(GpsUtil gpsUtil, RewardCentral rewardCentral) {
@@ -42,19 +42,55 @@ public class RewardsService {
 		proximityBuffer = defaultProximityBuffer;
 	}
 	
-	public void calculateReward(User user) {
+//	public void calculateReward(User user) {
+//		List<VisitedLocation> userLocations = user.getVisitedLocations().stream().collect(Collectors.toList());
+//		List<Attraction> attractions = gpsUtil.getAttractions();
+//
+//		for(VisitedLocation visitedLocation : userLocations) {
+//			for(Attraction attraction : attractions) {
+//				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
+//					if(nearAttraction(visitedLocation, attraction)) {
+//						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+//					}
+//				}
+//			}
+//		}
+//	}
+
+	public CompletableFuture<Void> calculateRewardss(User user) {
+		// Récupération des localisations visitées par l'utilisateur
 		List<VisitedLocation> userLocations = user.getVisitedLocations().stream().collect(Collectors.toList());
+
+		// Récupération de la liste des attractions depuis gpsUtil
 		List<Attraction> attractions = gpsUtil.getAttractions();
 
-		for(VisitedLocation visitedLocation : userLocations) {
-			for(Attraction attraction : attractions) {
-				if(user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
-					if(nearAttraction(visitedLocation, attraction)) {
-						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+		// Liste des CompletableFuture pour les tâches asynchrones
+		List<CompletableFuture<Void>> futures = new ArrayList<>();
+
+		// Parcours des localisations visitées par l'utilisateur
+		for (VisitedLocation visitedLocation : userLocations) {
+
+			// Parcours des attractions
+			for (Attraction attraction : attractions) {
+				// Vérification si l'utilisateur n'a pas déjà une récompense pour cette attraction
+				if (user.getUserRewards().stream().filter(r -> r.attraction.attractionName.equals(attraction.attractionName)).count() == 0) {
+					// Vérification si la localisation visitée est proche de l'attraction
+					if (nearAttraction(visitedLocation, attraction)) {
+						// Création d'un CompletableFuture pour ajouter la récompense à l'utilisateur
+						CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+							user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
+						}, executorService);
+
+						// Ajout du CompletableFuture à la liste des futures
+						futures.add(future);
+						break;
 					}
 				}
 			}
 		}
+
+		// Retourne un CompletableFuture qui sera complété lorsque tous les CompletableFuture dans la liste auront terminé
+		return CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
 	}
 
 	public CompletableFuture<Void> calculateRewards(User user) {
@@ -92,70 +128,6 @@ public class RewardsService {
 		// Retourne un CompletableFuture qui sera complété lorsque tous les CompletableFuture dans la liste auront terminé
 		return CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
 	}
-
-
-
-//	public void calculateRewards(User user) {
-//		List<VisitedLocation> userLocations = user.getVisitedLocations();
-//		List<Attraction> attractions = gpsUtil.getAttractions();
-//		List<Attraction> unvisitedAttractions = attractions.stream()
-//				.filter(attraction -> user.getUserRewards().stream()
-//						.noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName)))
-//				.collect(Collectors.toList());
-//
-//		ExecutorService executor = Executors.newFixedThreadPool(1000); // Definnir le nombre optimal de threads
-//
-//		userLocations.parallelStream().forEach(visitedLocation -> {
-//			unvisitedAttractions.parallelStream().forEach(attraction -> {
-//				if (nearAttraction(visitedLocation, attraction)) {
-//					executor.submit(() -> {
-//						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-//					});
-//				}
-//			});
-//		});
-//
-//		executor.shutdown();
-//		try {
-//			executor.awaitTermination(Long.MAX_VALUE, TimeUnit.NANOSECONDS);
-//		} catch (InterruptedException e) {
-//			// Gérer l'interruption
-//		}
-//	}
-
-//	public void calculateRewards(User user) {
-//		List<VisitedLocation> userLocations = user.getVisitedLocations();
-//		List<Attraction> attractions = gpsUtil.getAttractions();
-//		List<Attraction> unvisitedAttractions = attractions.stream()
-//				.filter(attraction -> user.getUserRewards().stream()
-//						.noneMatch(r -> r.attraction.attractionName.equals(attraction.attractionName)))
-//				.collect(Collectors.toList());
-//
-//		ExecutorService executor = Executors.newFixedThreadPool(4); // Définissez le nombre optimal de threads
-//
-//		List<CompletableFuture<Void>> completableFutures = new ArrayList<>();
-//
-//		for (VisitedLocation visitedLocation : userLocations) {
-//			for (Attraction attraction : unvisitedAttractions) {
-//				CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
-//					if (nearAttraction(visitedLocation, attraction)) {
-//						user.addUserReward(new UserReward(visitedLocation, attraction, getRewardPoints(attraction, user)));
-//					}
-//				}, executor);
-//				completableFutures.add(future);
-//			}
-//		}
-//
-//		CompletableFuture<Void> allTasks = CompletableFuture.allOf(completableFutures.toArray(new CompletableFuture[0]));
-//
-//		try {
-//			allTasks.get(); // Attendre la fin de toutes les tâches
-//		} catch (InterruptedException | ExecutionException e) {
-//			// Gérer les exceptions appropriées
-//		}
-//
-//		executor.shutdown();
-//	}
 
 
 
