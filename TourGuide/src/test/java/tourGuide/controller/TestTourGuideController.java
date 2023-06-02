7b36eba6c12e6536f7bbbc0f836374f83f6a3e6b
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-06-02 16:48:57 +0200

    implementation du premier test

--- TourGuide/src/test/java/tourGuide/controller/TestTourGuideController.java
+++ TourGuide/src/test/java/tourGuide/controller/TestTourGuideController.java
@@ -1,10 +1,31 @@
 package tourGuide.controller;
 
 
+import com.jsoniter.output.JsonStream;
+import gpsUtil.location.Location;
+import gpsUtil.location.VisitedLocation;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import tourGuide.model.user.User;
+import tourGuide.service.TourGuideService;
 
-@SpringBootTest
+import java.util.Date;
+import java.util.UUID;
+
+import static java.lang.reflect.Array.get;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest
 public class TestTourGuideController {
+
 
 
 
