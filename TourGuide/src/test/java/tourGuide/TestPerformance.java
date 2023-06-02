7b5ae2f842a3c0f0f990b9ee979a3f135acdead8
joselct17 <62754339+joselct17@users.noreply.github.com>
2023-06-02 08:30:54 +0200

    test artifacts

--- TourGuide/src/test/java/tourGuide/TestPerformance.java
+++ TourGuide/src/test/java/tourGuide/TestPerformance.java
@@ -76,7 +76,7 @@ public class TestPerformance {
 		GpsUtil gpsUtil = new GpsUtil();
 		RewardsService rewardsService = new RewardsService(gpsUtil, new RewardCentral());
 		// Users should be incremented up to 100,000, and test finishes within 20 minutes
-		InternalTestHelper.setInternalUserNumber(100000);
+		InternalTestHelper.setInternalUserNumber(10);
 		// Note that Tracker Thread is directly disabled thanks to stopTrackerAtStartup = true
 		TourGuideService tourGuideService = new TourGuideService(gpsUtil, rewardsService);
 		// Add the first attraction in GpsUtils internal list to all users:
